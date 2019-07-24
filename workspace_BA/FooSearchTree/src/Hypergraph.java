2edfee57aaada07508197bf8025fc09dfc2f1b43
Falko Hemstra <falko.hemstra@web.de>
2019-07-24 14:23:14 +0200

    Print Kosmetik.

--- workspace_BA/FooSearchTree/src/Hypergraph.java
+++ workspace_BA/FooSearchTree/src/Hypergraph.java
@@ -18,37 +18,35 @@ public class Hypergraph {
 	}
 
 	public String toOutputString() {
-		String res = "nodes: (";
+		String res = "nodes: {";
 		for (int i = 0; i < nodes.length; i++) {
 			res += nodes[i];
 			if (i < nodes.length - 1)
 				res += ",";
 			else
-				res += ")\n";
+				res += "}\n";
 		}
-		res += "edges: ";
+		res += "edges: {";
 		for (int i = 0; i < edges.size(); i++) {
 			Tuple t = edges.get(i);
-			String tmp = t.toOutputString();
-			tmp = tmp.replaceAll("\\(", "\\{").replaceAll("\\)", "\\}").replaceAll("\\|", "\\,");
-			res += tmp;
-			res = (i < edges.size()-1) ? res + ";" : res;
+			res += t.toOutputString();
+			res = (i < edges.size()-1) ? res + "," : res;
 		}
-		res += "\n";
+		res += "}\n";
 		res += "mapping:\n";
 		// Loop through all mappings from nodes to edges
 		for (Entry<Integer, ArrayList<Tuple>> e : node_to_edges.entrySet()) {
 			res += " " + e.getKey() + " -> ";
 			// Loop through all edges the entry e is contained in
 			for (int i = 0; i < e.getValue().size(); i++) {
-				res += "{";
+				res += "(";
 				// Loop through all nodes that are in an edge
 				for (int j = 0; j < e.getValue().get(i).elements.length; j++) {
 					res += Integer.toString(e.getValue().get(i).elements[j]);
 					if (j < e.getValue().get(i).elements.length - 1)
-						res += ",";
+						res += "|";
 				}
-				res += "}; ";
+				res += "),";
 			}
 			res += "\n"; // TODO fix this for last iteration
 		}
