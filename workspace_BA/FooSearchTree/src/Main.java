24b7eb028a00855e5a02a8034f2b47215843eb62
Falko Hemstra <falko.hemstra@web.de>
2019-07-27 19:49:23 +0200

    Prints.

--- workspace_BA/FooSearchTree/src/Main.java
+++ workspace_BA/FooSearchTree/src/Main.java
@@ -28,6 +28,7 @@ public class Main {
 			System.out.println("~~~~~~~~~~~~~~~~");
 			// Kernelization
 			Hypergraph kernel = hyp.kernelize(hyp, form.k_par);
+			System.out.println("<< Main\nKernel:");
 			System.out.println(kernel.toOutputString());
 			System.out.println("++++++++++++++++");
 			/*
