1d6ce75f6c7cb95eaac8b77c61365d67cda852d7
Falko Hemstra <falko.hemstra@web.de>
2019-09-18 07:45:02 +0200

    Fünf Minuten reichen für keinen Graph aus PACE.

--- matlab_plots/pace/cactus_graph.m
+++ matlab_plots/pace/cactus_graph.m
@@ -1,6 +1,6 @@
 close all
 
-files = dir('*.csv');
+files = dir('*all*.csv');
 file = files(1);
 file_content = importdata(file.name);
 
@@ -8,7 +8,7 @@ figure
 hold on;
 x = file_content.data(:,3); % pipe 2 time
 y = file_content.data(:,9); % pipe 2 results
-y = cumsum(y) % prefixsum
+y = cumsum(y); % prefixsum
 plot(x,y);
 title 'Zahl der gelösten Instanzen innerhalb des timeouts';
 xlabel 'Zeit';
