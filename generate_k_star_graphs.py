e0bf8ae3f0e25c5cd7be381c98592d9b4d152ac2
Falko Hemstra <falko.hemstra@web.de>
2019-09-23 16:41:30 +0200

    Test auf k_star Graphen.
    Code ist noch nicht korrekt.

--- generate_k_star_graphs.py
+++ generate_k_star_graphs.py
@@ -3,11 +3,11 @@ import random
 
 # Creates random graphs which contain a vertex-cover of size k 
 
-probability = 0.075
+probability = 0.02
 variance_count = 10
-k_par = 3
+k_par = 60
 
-for nr_of_nodes in range(50,51,200):
+for nr_of_nodes in range(200,800,200):
 	# create directory if it does not exist yet
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	dest_dir = dir_path + os.sep + 'workspace_BA' + os.sep + 'FooSearchTree' + os.sep + 'input_graphs' + os.sep + 'k_star_graphs'
