521fb9c181cb915f221dd88cd18a2cd0d80ebb02
Falko Hemstra <falko.hemstra@web.de>
2019-09-25 19:42:25 +0200

    G(n,m) Test Setup.

--- generate_gnm_graphs.py
+++ generate_gnm_graphs.py
@@ -1,8 +1,6 @@
 import os
 import networkx as nx
 
-n = 200
-m = 200
 variance_count = 10
 
 # create directory if it does not exist yet
@@ -10,18 +8,19 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 dest_dir = dir_path + os.sep + 'workspace_BA' + os.sep + 'FooSearchTree' + os.sep + 'input_graphs' + os.sep + 'gnm_graphs'
 if not os.path.exists(dest_dir):
 	os.makedirs(dest_dir) 
+for n in range(20,520,20):
+	m = n
+	for i in range(variance_count):
+		graph = nx.gnm_random_graph(n, m)
+		nodes = graph.nodes()
+		edges = graph.edges()
+		density = len(edges)/len(nodes)
 
-for i in range(variance_count):
-	graph = nx.gnm_random_graph(n, m)
-	nodes = graph.nodes()
-	edges = graph.edges()
-	density = len(edges)/len(nodes)
-
-	filename = "gnm_n_" + str(n) + "_m_" + str(m) + "_" + str(i) + ".gr"
-	print(str(filename) + ", dens: " + str(density))
-	full_path = dest_dir + os.sep + filename
-	with open(full_path, 'w') as curr_file:
-		header = "p td " + str(len(nodes)) + " " + str(len(edges))
-		curr_file.write(str(header) + '\n')
-		for edge in edges:
-			curr_file.write(str(edge[0]) + " " + str(edge[1]) + "\n")
+		filename = "gnm_n_" + str(n) + "_m_" + str(m) + "_" + str(i) + ".gr"
+		print(str(filename) + ", dens: " + str(density))
+		full_path = dest_dir + os.sep + filename
+		with open(full_path, 'w') as curr_file:
+			header = "p td " + str(len(nodes)) + " " + str(len(edges))
+			curr_file.write(str(header) + '\n')
+			for edge in edges:
+				curr_file.write(str(edge[0]) + " " + str(edge[1]) + "\n")
