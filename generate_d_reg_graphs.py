caf472adb22916e1f9d1307ecdd48a553ffe64bc
Falko Hemstra <falko.hemstra@web.de>
2019-10-16 17:07:23 +0200

    Einige d-reguläre Graphen hinzugefügt.

--- generate_d_reg_graphs.py
+++ generate_d_reg_graphs.py
@@ -1,15 +1,15 @@
 import os
 import networkx as nx
 
-variance_count = 5
-d = 3
+variance_count = 3
+d = 4
 
 # create directory if it does not exist yet
 dir_path = os.path.dirname(os.path.realpath(__file__))
-dest_dir = dir_path + os.sep + 'workspace_BA' + os.sep + 'ba-solver' + os.sep + 'src' + os.sep + 'main' + os.sep + 'resources'+ os.sep + 'input_graphs' + os.sep + 'd_reg_graphs'
+dest_dir = dir_path + os.sep + 'd_reg_graphs'
 if not os.path.exists(dest_dir):
 	os.makedirs(dest_dir) 
-for n in range(10,110,10):
+for n in range(10,50,10):
 	for i in range(variance_count):
 		graph = nx.random_regular_graph(d, n)
 		nodes = graph.nodes()
