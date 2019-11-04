6ca7bed96b8e4a32ec13d8a9d94169d13edf5b4a
Falko Hemstra <falko.hemstra@web.de>
2019-11-04 23:09:23 +0100

    Typo.

--- test_this.sh
+++ test_this.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar ba-solver-1.0.jar -f $PWD/test_setup/test_formula -g $PWD/test_setup/test_graphs -gu  -ke -bo -heu -be
+java -jar ba-solver-1.0.jar -f $PWD/test_setup/test_formula -g $PWD/test_setup/test_graphs -ke -be -bo -heu -t 2
 echo "------------"
